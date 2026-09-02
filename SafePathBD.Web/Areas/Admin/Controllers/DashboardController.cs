79254e8f1aeb42f8a673949897413f96833611e5
Samanta503 <samsamanta357@gmail.com>
2026-09-02 06:23:27 +0600

    Remove DashboardController and related view model

--- SafePathBD.Web/Areas/Admin/Controllers/DashboardController.cs
+++ SafePathBD.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -5,34 +5,3 @@ using SafePathBD.Web.Models.DTOs.Moderation;
 using SafePathBD.Web.Security;
 using SafePathBD.Web.Services.Interfaces;
 
-namespace SafePathBD.Web.Areas.Admin.Controllers;
-
-[Area("Admin")]
-[Authorize(Roles = RoleNames.Admin)]
-public class DashboardController : Controller
-{
-    private readonly IReportModerationService _moderation;
-
-    public DashboardController(IReportModerationService moderation)
-    {
-        _moderation = moderation;
-    }
-
-    public async Task<IActionResult> Index(CancellationToken cancellationToken)
-    {
-        ViewData["AdminName"] = User.GetDisplayName();
-
-        return View(new AdminOverviewViewModel
-        {
-            Counts = await _moderation.GetCountsAsync(cancellationToken),
-            RecentActions = await _moderation.GetRecentActionsAsync(8, cancellationToken)
-        });
-    }
-}
-
-public sealed class AdminOverviewViewModel
-{
-    public ModerationCountsDto Counts { get; init; } = new(0, 0, 0, 0, 0, 0, 0);
-
-    public IReadOnlyList<AdminActionEntryDto> RecentActions { get; init; } = Array.Empty<AdminActionEntryDto>();
-}
